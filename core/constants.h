6e50d9b8a31a474ff269808709210962a9ce0029
yang172234 <3535289201@qq.com>
2026-04-28 19:25:12 +0800

    Create constants.h

---
+++ core/constants.h
@@ -0,0 +1,21 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define MAX_SKILLS 4
+#define NAME_LENGTH 32
+#define MAX_STATUS_TURNS 5
+
+#define SLEEP_MIN_TURNS 3
+#define SLEEP_MAX_TURNS 5
+#define STATUS_DAMAGE_FRACTION 16
+
+// 战斗常量
+#define BASE_LEVEL 5
+#define EXP_TO_NEXT 100
+
+// 伤害公式常量
+#define CRIT_MULTIPLIER 1.5f
+#define DAMAGE_MIN_RANDOM 0.85f
+#define DAMAGE_MAX_RANDOM 1.0f
+
+#endif
